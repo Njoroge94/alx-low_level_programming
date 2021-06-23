04bfed58efddca605ed1f446c6b2ae5dc8c1a56a
Njoroge94 <rimorearchie04@outlook.com>
2021-06-23 06:23:46 -0700

    natural numbers only

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ *print_to_98 - all natural numbers
+ *@n: the starting digit/number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i == 98)
+		{
+			break;
+		}
+		printf(", ");
+	}
+	printf("\n");
+}
