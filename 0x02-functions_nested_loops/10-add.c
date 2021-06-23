a55d44d2943565287f7b2572dca1c15e2467820f
Njoroge94 <rimorearchie04@outlook.com>
2021-06-23 05:51:49 -0700

    adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ *add - the add prototype
+ *@r: the number
+ *@n: the digit
+ *Return: Always 0 (success);
+ */
+int add(int r, int n)
+{
+	int sum;
+
+	sum = r + n;
+	return (sum);
+}
