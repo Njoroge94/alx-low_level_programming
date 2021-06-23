599dbae0486698abf2ab4ff0e3e6191b93b8728a
Njoroge94 <rimorearchie04@outlook.com>
2021-06-23 07:03:41 -0700

    all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,30 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		printf("%d", i);
-		if (i == 98)
+		for (i = n; i <= 98; i++)
 		{
-			break;
+			printf("%d", i);
+			if (i == 98)
+			{
+				break;
+			}
+			printf(", ");
 		}
-		printf(", ");
+		printf("\n");
 	}
-	printf("\n");
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{
+				break;
+			}
+			printf(", ");
+		}
+		printf("\n");
+	}
 }
