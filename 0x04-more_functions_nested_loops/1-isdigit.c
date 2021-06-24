78063072ace722f7cc8c328a738a95bf3871cadf
Njoroge94 <rimorearchie04@outlook.com>
2021-06-24 10:19:03 -0700

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ *_isdigit - checking for digits
+ *@c: the element
+ *Return: Always 0 (success)
+ */
+int _isdigit(int c)
+{
+	int digit;
+
+	if ((c >= 0 || c == '0') && c <= 9)
+	{
+		digit = 1;
+	}
+	else
+	{
+		digit = 0;
+	}
+	return (digit);
+}
