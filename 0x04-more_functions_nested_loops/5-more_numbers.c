59179482d4fb52cbc9e9b53b80bf6292c5a7d5af
Njoroge94 <rimorearchie04@outlook.com>
2021-06-25 10:46:41 -0700

    greater than 9 with putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,15 @@
 void more_numbers(void)
 {
 	int no;
-	int count, ch;
+	int count;
 
 	for (count = 0; count < 10; count++)
 	{
-		for (no = 0; no < 10; no++)
+		for (no = 0; no < 15; no++)
 		{
 			if (no > 9)
-			{
-				_putchar(1 + '0')
-				_putchar(no % 10);
+				_putchar(1 + '0');
+			_putchar((no % 10) + '0');
 		}
 		_putchar('\n');
 	}
