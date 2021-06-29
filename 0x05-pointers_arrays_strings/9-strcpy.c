5432711a6d9316bab618022049aae4f03bf3e5d1
Njoroge94 <rimorearchie04@outlook.com>
2021-06-29 12:08:46 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *_strcpy - copying a string to another
+ *@dest: the destination of the copied string
+ *@src: the source of string to be copied
+ *Return: Always 0 (success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	/*memset(dest, '\0', sizeof(dest));*/
+	_strcpy(src, ptr);
+	_strcpy(dest, src);
+
+	printf("%s", dest);
+
+	return (dest);
+}
