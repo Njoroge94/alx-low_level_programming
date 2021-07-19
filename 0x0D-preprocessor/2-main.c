46e8ef49967c01564928bc865d713f49177f48a7
Njoroge94 <rimorearchie94@outlook.com>
2021-07-19 06:02:03 -0700

    own function-like macro of ABS(x)

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: the count of commandline arguments
+ *@argv: the string with the arguments
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}
