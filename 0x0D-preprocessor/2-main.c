87a152c665ac0f5732cec1890ee51c1cce62a01d
Njoroge94 <rimorearchie94@outlook.com>
2021-07-19 13:27:31 -0700

    printing name of the source file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *@argc: the count of commandline arguments
- *@argv: the string with the arguments
  *Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	printf("%s\n", argv[0]);
-	(void)argc;
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
