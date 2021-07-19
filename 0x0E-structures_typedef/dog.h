b3e03d065782ef857c87869cded4d25e1a86bc84
Njoroge94 <rimorearchie94@outlook.com>
2021-07-19 07:22:32 -0700

    new type struct dog with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef POPPY_H
+#define POPPY_H
+/**
+ *struct dog - the structure
+ *@name: the dogs name
+ *@age: its age
+ *@owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* POPPY_H */
